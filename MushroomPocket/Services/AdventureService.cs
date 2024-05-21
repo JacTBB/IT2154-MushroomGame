8050174b8366f270310a484f160b89eb27ec8f80
JacTBB <jackiesoon66@gmail.com>
2024-05-21 19:20:25 +0800

    Powerups, Fight Choices, Revamped attack logic

--- MushroomPocket/Services/AdventureService.cs
+++ MushroomPocket/Services/AdventureService.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -65,6 +67,9 @@ namespace MushroomPocket.Services
             }
         }
 
+        /// <summary>
+        /// Give random amount of coins & EXP
+        /// </summary>
         public void FoundCoin()
         {
             int coins = random.Next(2, 5);
@@ -93,6 +98,7 @@ namespace MushroomPocket.Services
         /// </summary>
         public void Fight()
         {
+            #region Get Enemy Character
             List<Character> AliveEnemies = EnemyCharacters.Where(c => c.HP > 0).ToList();
             if (AliveEnemies.Count == 0)
             {
@@ -112,6 +118,7 @@ namespace MushroomPocket.Services
                 };
                 return;
             }
+            #endregion
 
             Character EnemyCharacter = AliveEnemies[random.Next(0, AliveEnemies.Count() - 1)];
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -119,55 +126,179 @@ namespace MushroomPocket.Services
             Console.ResetColor();
             Thread.Sleep(1000);
 
+            int FriendlyStrengthBoost = 0;
+
             while (character.HP > 0 && EnemyCharacter.HP > 0)
             {
-                // Friendly Attack
-                int CharacterDamage = SkillDamages[character.Skill];
-                int DamageBoost = (int)Math.Floor((double)character.EXP / 100) * 5;
-                EnemyCharacter.HP = Math.Max(0, EnemyCharacter.HP - (CharacterDamage + DamageBoost));
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{character.Name} used {character.Skill} and dealt {CharacterDamage + DamageBoost}");
-                Console.ResetColor();
-                Console.WriteLine($"{EnemyCharacter.Name} is now at {EnemyCharacter.HP} HP");
-                Thread.Sleep(1000);
+                string FriendlyFightChoice = FightChoice();
 
-                if (EnemyCharacter.HP <= 0)
+                string EnemyFightChoice = "Attack";
+
+                if (FriendlyFightChoice == "Run")
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"{EnemyCharacter.Name} died, you won!");
-                    Console.ResetColor();
-                    int exp = random.Next(20, 50);
-                    character.EXP += exp;
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine($"{character.Name} gained {exp} EXP.");
-                    Console.ResetColor();
+                    Console.WriteLine($"{character.Name} ran away");
                     break;
                 }
+
+                if (FriendlyFightChoice != "Attack" && FriendlyFightChoice != "Doge")
+                {
+                    switch (FriendlyFightChoice)
+                    {
+                        case "Health Potion":
+                            character.HP += 150;
+                            pocketContext.SaveChanges();
+                            break;
+                        case "Experience Potion":
+                            character.EXP += 100;
+                            pocketContext.SaveChanges();
+                            break;
+                        case "Strength Potion":
+                            FriendlyStrengthBoost = 50;
+                            break;
+                        default:
+                            FriendlyFightChoice = FightChoice();
+                            break;
+                    }
+                }
+
+                // Friendly Attack
+                if (FriendlyFightChoice == "Attack")
+                {
+                    Attack(character, EnemyCharacter, false, true, FriendlyStrengthBoost);
+                    if (FriendlyStrengthBoost > 0)
+                    {
+                        FriendlyStrengthBoost = 0;
+                    }
+                }
+                Thread.Sleep(1000);
 
                 // Enemy Attack
-                int EnemyDamage = SkillDamages[EnemyCharacter.Skill];
-                character.HP = Math.Max(0, character.HP - EnemyDamage);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{EnemyCharacter.Name} used {EnemyCharacter.Skill} and dealt {SkillDamages[EnemyCharacter.Skill]}");
-                Console.ResetColor();
-                Console.WriteLine($"{character.Name} is now at {character.HP} HP");
-                Thread.Sleep(1000);
-
-                if (character.HP <= 0)
+                if (EnemyFightChoice == "Attack")
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{character.Name} died, you lost!");
-                    Console.ResetColor();
-                    int exp = random.Next(5, 10);
-                    character.EXP += exp;
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine($"{character.Name} gained {exp} EXP.");
-                    Console.ResetColor();
-                    break;
+                    Attack(EnemyCharacter, character, FriendlyFightChoice == "Doge", false, 0);
                 }
+                Thread.Sleep(1000);
             }
 
             pocketContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get fight choice. if inventory selected, consume item.
+        /// </summary>
+        private string FightChoice()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("(1) Attack");
+            Console.WriteLine("(2) Doge");
+            Console.WriteLine("(3) Inventory");
+            Console.WriteLine("(4) Run away");
+
+            int index;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out index)) { Console.WriteLine("Invalid number!"); continue; }
+                if (index < 1 || index > 4) { Console.WriteLine("Index must be between 1 and 4!"); continue; }
+                break;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    return "Attack";
+                case 2:
+                    return "Doge";
+                case 3:
+                    // List Inventory Items
+                    if (pocketContext.Inventory.Count() == 0)
+                    {
+                        Console.WriteLine("You have no items!");
+                        return FightChoice();
+                    }
+
+                    Dictionary<string, int> counter = new Dictionary<string, int>();
+                    foreach (Item item in pocketContext.Inventory.ToList())
+                    {
+                        if (counter.ContainsKey(item.Name))
+                        {
+                            counter[item.Name]++;
+                        }
+                        else
+                        {
+                            counter.Add(item.Name, 1);
+                        }
+                    }
+
+                    Console.WriteLine("\nItems:");
+                    foreach (KeyValuePair<string, int> item in counter.ToList())
+                    {
+                        Console.WriteLine($"({counter.ToList().IndexOf(item)}) {item.Value} {item.Key}");
+                    }
+                    Console.WriteLine("B to return to fight menu");
+
+                    int itemIndex;
+                    while (true)
+                    {
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "b") { return FightChoice(); }
+                        if (!int.TryParse(input, out itemIndex)) { Console.WriteLine("Invalid number!"); continue; }
+                        if (itemIndex < 0 || itemIndex > pocketContext.Inventory.Count()) { Console.WriteLine($"Index must be between 0 and {pocketContext.Inventory.Count()}!"); continue; }
+                        break;
+                    }
+                    string itemChoice = counter.ToList().ElementAt(itemIndex).Key;
+                    Console.WriteLine($"You used {itemChoice}");
+
+                    pocketContext.Remove(pocketContext.Inventory.FirstOrDefault(i => i.Name == itemChoice));
+
+                    return itemChoice;
+                case 4:
+                    return "Run";
+                default:
+                    return FightChoice();
+            }
+        }
+
+        /// <summary>
+        /// Attack Damage logic
+        /// </summary>
+        private void Attack(Character char1, Character char2, bool Doge, bool GrantEXP, int StrengthBoost)
+        {
+            int Dmg = SkillDamages[char1.Skill];
+            int DmgBoost = (int)Math.Floor((double)char1.EXP / 100) * 5;
+            double DogeMultiplier = 1;
+
+            int DogeRandom = random.Next(0, 3);
+            if (Doge)
+            {
+                if (DogeRandom == 0)
+                {
+                    Console.WriteLine($"{char2.Name} fully doged the attack");
+                    DogeMultiplier = 0;
+                }
+                else
+                {
+                    Console.WriteLine($"{char2.Name} partially doged the attack");
+                    DogeMultiplier = 0.5;
+                }
+            }
+
+            int DmgDealt = (int)Math.Round((Dmg + DmgBoost) * DogeMultiplier);
+            char2.HP = Math.Max(0, char2.HP - DmgDealt);
+            Console.WriteLine($"{char1.Name} used {char1.Skill} and dealt {DmgDealt}");
+            Console.WriteLine($"{char2.Name} is now at {char2.HP} HP");
+
+            Thread.Sleep(500);
+            if (char2.HP <= 0)
+            {
+                Console.WriteLine($"{char2.Name} died, {char1.Name} won!");
+                if (GrantEXP)
+                {
+                    int exp = random.Next(20, 50);
+                    char1.EXP += exp;
+                    Console.WriteLine($"{char1.Name} gained {exp} EXP.");
+                }
+            }
         }
     }
 }
